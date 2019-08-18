25ff30e2a4a14b28b4be0671fb9a8812dd15a73e
Swastik Shrestha <40354116+Shrestha7@users.noreply.github.com>
2019-08-18 08:34:59 +0545

    created project medikart

--- MediKart/wsgi.py
+++ MediKart/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for medikart project.
+WSGI config for Medikart project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'medikart.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Medikart.settings')
 
 application = get_wsgi_application()
